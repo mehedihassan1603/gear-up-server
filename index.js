0cd44b6cdd1fa9f62bba2cbbc2790586224a62e3
Mehedi Hassan <mh93075@gmail.com>
2023-10-20 00:30:49 +0600

    Read all the from database

--- index.js
+++ index.js
@@ -34,17 +34,17 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/addcart', async(req, res)=>{
-      const cursor = addcartCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-
     app.get('/product/:id', async(req, res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const product = await productCollection.findOne(query);
       res.send(product);
+    })
+    
+    app.get('/addcart', async(req, res)=>{
+      const cursor = addcartCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
     app.post('/product', async(req, res)=>{
