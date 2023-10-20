08b63e4b10b051bc159838c228266060b7a17d36
Mehedi Hassan <mh93075@gmail.com>
2023-10-20 23:39:43 +0600

    solve a issue

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    
 
     const productCollection = client.db('productDB').collection('product');
     const addcartCollection = client.db('productDB').collection('addcart');
